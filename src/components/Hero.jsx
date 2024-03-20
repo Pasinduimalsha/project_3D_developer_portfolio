bf6a4588ad7a9fee7fce5953db3e908809e89a3b
Pasindu Mallawarachchi <142653028+Pasinduimalsha@users.noreply.github.com>
2024-03-20 18:59:39 +0530

    Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,29 +5,36 @@ import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <section className={`relative w-full h-screen mx-auto`}>
-      <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
-        <div className='flex flex-col justify-center items-center mt-5'>
+    <section className={`relative w-full h-screen mx-auto flex flex-col :flex-row justify-between items-center`} >
+      <div className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-col md:flex-row items-start md:items-center gap-5`}>
+
+        <div className='flex flex-row justify-center items-center '>
+        <div className='flex flex-col justify-center items-center '>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
-
+         
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Pasindu Mallawarachchi</span> 
+           
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          <p className={`${styles.heroSubText} text-white-100`}>
             I develop 3D visuals, user <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
+        </div>
+        
+        <div className='flex justify-center items-center '>
+        <ComputersCanvas />
+        </div>
+         
       </div>
 
-      <ComputersCanvas />
+     
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center '>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
